f7d2b869a2a28ba7800386bfd509e243bf5f5457
yyrcat <yeruidacz@gmail.com>
2017-05-23 21:57:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,20 @@
-(function() {
-var controls = document.querySelectorAll('.evo_c-scrolling-carousel_controller');
-var slides = document.querySelectorAll(".evo_c-scrolling-carousel_item");
-  var slideContainer = document.querySelector('.evo_c-scrolling-nav-carousel');
-  var currentSlide = slides.length - 1;
-  var notifier = document.querySelectorAll('.evo_c-scrolling-carousel_notifier li');
+var controls = document.querySelectorAll('.evo_c-scrolling-carousel_controller'),
+  slides = document.querySelectorAll(".evo_c-scrolling-carousel_item"),
+  currentSlide = slides.length - 1,
+  next = document.querySelector(".evo_c-scrolling-carousel_controller--next"),
+  fallBack = document.querySelector(".evo_c-scrolling-carousel_controller--return"),
+  notifier = document.querySelectorAll('.evo_c-scrolling-carousel_notifier li');
 
-slideContainer.addEventListener("wheel", controlSlider);
-
-function controlSlider(e) {
-  var e = window.event || e;
-  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-  if (delta > 0) {
-  	returnSlide();
-  } else {
-     goToSlide(currentSlide - 1);
-  }
+function nextSlide() {
+  goToSlide(currentSlide - 1);
 }
-
-
-/*window.onscroll = function() {
-  controlSlide(slideContainer);
-};
-
-function controlSlide(el) {
-  var yPos = 0,
-    elHeight = el.clientHeight;
-  while (el) {
-    if (el.tagName == "BODY") {
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
-
-    el = el.offsetParent;
-  }
-  if (yScroll - yPos > elHeight) {
-    goToSlide(currentSlide - 1);
-  } else {
-    returnSlide();
-  }
-}
-*/
-
 
 function returnSlide() {
   for (var i = 0; i < slides.length; i++) {
     slides[i].className = "evo_c-scrolling-carousel_item";
   }
   currentSlide = slides.length - 1;
-  for (var i = 0; i < notifier.length; i++) {
+   for (var i = 0; i < notifier.length; i++) {
     notifier[i].className = "";
   }
   notifier[0].className = "is-current";
@@ -59,7 +23,7 @@ function returnSlide() {
 function goToSlide(n) {
   slides[currentSlide].className = "evo_c-scrolling-carousel_item is-pre";
   notifier[slides.length-1-currentSlide].className = "";
-  if (n >= 0) {
+   if (n >= 0) {
     notifier[slides.length-1-n].className = "is-current";
     if (n >= 1) {
       currentSlide = n;
@@ -68,4 +32,5 @@ function goToSlide(n) {
 
   }
 }
-})();
+fallBack.addEventListener('click', returnSlide, false);
+next.addEventListener('click', nextSlide, false);
